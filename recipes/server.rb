85d1b6e4b71b0be07105d298b9e36570177a97e3
Rodrigo Pichinual <rodrigo.pichinual@gmail.com>
2019-12-10 18:49:48 +0000

    Index.html includes node atributes for ipaddress and hostname

--- recipes/server.rb
+++ recipes/server.rb
@@ -1,7 +1,10 @@
 package 'httpd'
 
 file '/var/www/html/index.html' do
-	content '<h2> Hola Mundo Cruel ! jejejej </h2>'
+	content "<h2> Hola Mundo Cruel ! jejejej </h2>
+	<h2> IPADDRESS: #{node['ipaddress']} </h2>
+	<h2> HOSTANAME: #{node['hostname']} </h2>
+"
 end
 
 service 'httpd' do
